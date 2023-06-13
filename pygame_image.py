b84af0cdc958aef83ebd823b2aefd55b22342763
mitsuhashi <c0a22132df@edu.teu.ac.jp>
2023-06-13 17:20:54 +0900

    演習ｖ1

--- pygame_image.py
+++ pygame_image.py
@@ -7,43 +7,32 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    bgReverse_img = pg.transform.flip(bg_img, True, False)
-    koukaton_img = pg.image.load("ex01/fig/3.png")
-    koukaton2_img = pg.image.load("ex01/fig/4.png")
-    koukaton2R_img = pg.transform.flip(koukaton2_img, True, False)
-    koukatonR_img = pg.transform.flip(koukaton_img, True, False)
-    koukatonRR_img = [koukaton2R_img, pg.transform.rotate(koukatonR_img, 10)]
+    bg_imgs = [bg_img,pg.transform.flip(bg_img, True, False)]
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img2 = pg.image.load("ex01/fig/4.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_img2 = pg.transform.flip(kk_img2,True,False)
     x = 0
-    x2 = 0
-
+    
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img, 10, 1.0),kk_img,pg.transform.rotozoom(kk_img2, 0, 1.0)]
+    
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        ##
 
-        
-        
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bgReverse_img, [1600-x2, 0])
-        screen.blit(bg_img, [3200-x2, 0])
-        if tmr <= 40:
-            screen.blit(koukatonRR_img[0], [300, 200])
-        else:
-            screen.blit(koukatonRR_img[1], [300, 200])
+        print(x)
+        screen.blit(bg_imgs[tmr % 1],[-tmr, 0])
+
+        #screen.blit(bg_imgs[0],[0, 0])
+
+
+        screen.blit(kk_imgs[tmr % len(kk_imgs)],[300, 200])
         pg.display.update()
         tmr += 1
-
-        if tmr >= 60:
-            tmr=0
-        x += 1
-        if x >= 1600:
-            x = 0
-        x2 += 1
-        if x2 >= 6200:
-            x2 -= 6200   
-        clock.tick(200)
-
+        clock.tick(10)
 
 
 if __name__ == "__main__":
